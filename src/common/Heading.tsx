062f8ca038771a350960442fa54e22a802484ce8
LEE SUL <suerish.e@gmail.com>
2024-06-22 02:16:00 +0900

    fix: 공통 Heading props className 확장

--- src/common/Heading.tsx
+++ src/common/Heading.tsx
@@ -1,14 +1,15 @@
 type HeadingProps = {
-  level: "h1" | "h2" | "h3" | "h4";
+  level: "1" | "2" | "3" | "4";
   weight: "extrabold" | "bold" | "medium";
+  className?: string;
   children: React.ReactNode;
 };
 
 const leveltVariants = {
-  h1: "text-[60px] leading-[72px] -tracking-[0.01em]",
-  h2: "text-[48px] leading-[140%] tracking-normal",
-  h3: "text-[36px] leading-[40px] tracking-normal",
-  h4: "text-[30px] leading-[36px] tracking-normal",
+  1: "text-[60px] leading-[72px] -tracking-[0.01em]",
+  2: "text-[48px] leading-[140%] tracking-normal",
+  3: "text-[36px] leading-[40px] tracking-normal",
+  4: "text-[30px] leading-[36px] tracking-normal",
 };
 const weightVariants = {
   extrabold: "font-extrabold",
@@ -16,12 +17,19 @@ const weightVariants = {
   medium: "font-medium",
 };
 
-export default function Heading({ level, weight, children }: HeadingProps) {
-  const Tag = level;
+export default function Heading({
+  level,
+  weight,
+  className,
+  children,
+}: HeadingProps) {
+  const Tag = `h${level}` as keyof JSX.IntrinsicElements;
 
   return (
     <>
-      <Tag className={`${weightVariants[weight]} ${leveltVariants[level]}`}>
+      <Tag
+        className={`${weightVariants[weight]} ${leveltVariants[level]} ${className}`}
+      >
         {children}
       </Tag>
     </>
