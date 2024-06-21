5a7d29cf73534c52f1196ab130610b1aab7a6ae7
LEE SUL <suerish.e@gmail.com>
2024-06-22 02:17:21 +0900

    add: 공통 Body 폰트 컴포넌트 추가

---
+++ src/common/Body.tsx
@@ -0,0 +1,36 @@
+type BodyProps = {
+  level: "1" | "2" | "3" | "4" | "5";
+  weight: "bold" | "medium" | "regular";
+  className?: string;
+  children: React.ReactNode;
+};
+
+const leveltVariants = {
+  1: "text-[24px] leading-[32px] tracking-normal",
+  2: "text-[20px] leading-[28px] tracking-normal",
+  3: "text-[18px] leading-[28px] tracking-normal",
+  4: "text-[16px] leading-[24px] tracking-normal",
+  5: "text-[14px] leading-[20px] tracking-normal",
+};
+const weightVariants = {
+  bold: "font-bold",
+  medium: "font-medium",
+  regular: "font-normal",
+};
+
+export default function Body({
+  level,
+  weight,
+  className,
+  children,
+}: BodyProps) {
+  return (
+    <>
+      <p
+        className={`${weightVariants[weight]} ${leveltVariants[level]} ${className}`}
+      >
+        {children}
+      </p>
+    </>
+  );
+}
