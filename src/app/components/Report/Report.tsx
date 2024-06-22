b361606702c78d9399fc5a279dcbeb32234d4672
Jo Young <githuborigin@gmail.com>
2024-06-23 02:46:59 +0900

    feat: component 생성 및 report 폴더 생성

---
+++ src/app/components/Report/Report.tsx
@@ -0,0 +1,16 @@
+import CardWrap from "@/common/CardWrap";
+import React from "react";
+
+export default function Report() {
+  return (
+    <div className="w-full h-[1000px] ">
+      <div className="h-[300px] w-[300px]">
+        <CardWrap>
+          <h1>
+            안녕하세요안녕하세요안녕하세요안녕하세요안녕하세요안녕하세요안녕하세요안녕하세요안녕하세요안녕하세요
+          </h1>
+        </CardWrap>
+      </div>
+    </div>
+  );
+}
