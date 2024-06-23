1abac7e2160b156adacc46a997037f122f193a6d
Jo Young <githuborigin@gmail.com>
2024-06-24 00:05:47 +0900

    feat: 공용 카드 랩 컴포넌트 생성

---
+++ src/common/CardWrap.tsx
@@ -0,0 +1,38 @@
+interface Props {
+  bgColor?: string;
+  children?: React.ReactNode;
+  width?: string;
+  height?: string;
+}
+/**
+ * 기본 배경 색상이 white인 라운드 2xl(16px) 카드 컴포넌트
+ *  배경색, 가로 , 높이 , children 설정 을 인자로 받음 모두 옵셔널
+ *  가로 최소 320px 지정 width값이 320보다 작아도 320으로 고정
+ * @param {string | null} bgColor - 카드 배경 색상
+ * @param {string | null} width - 카드 너비
+ * @param {string | null} height - 카드 높이
+ * @param {React.ReactNode | null} children - 카드 안에 들어갈 내용
+ * 사용 예시 : <CardWrap bgColor="red" width="320px" height="240px">내용</CardWrap>
+ */
+export default function CardWrap({
+  bgColor = "white",
+  width = "320px",
+  height = "240px",
+  children,
+}: Props) {
+  return (
+    <>
+      {/* 기본 스타일  배경은 흰색, 라운드는 2xl, 패딩은 8*/}
+      <article
+        className=" rounded-2xl p-8  min-w-[320px]"
+        style={{
+          backgroundColor: bgColor,
+          width,
+          height,
+        }}
+      >
+        {children}
+      </article>
+    </>
+  );
+}
