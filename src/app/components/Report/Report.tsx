f7bd4b317c3fb946ec198a18130f5717f67dfdd3
Jo Young <githuborigin@gmail.com>
2024-06-24 17:22:20 +0900

    refactor : 최소 가로 너비 반응형 설정

--- src/app/components/Report/Report.tsx
+++ src/app/components/Report/Report.tsx
@@ -1,15 +1,59 @@
 import CardWrap from "@/common/CardWrap";
-import React from "react";
+import StockIcon from "./Stock/StockIcon";
+import BodyFont from "@/common/BodyFont";
+import HeadingFont from "@/common/HeadingFont";
+import TextButton from "@/common/TextButton";
+import StockAnalysis from "./Stock/StockAnalysis";
+import StockSumary from "./Stock/StockSumary";
+import StockChart from "./Stock/StockChart";
+import StockReport from "./Stock/StockReport";
+import StockHeader from "./Stock/StockHeader";
 
-export default function Report() {
+interface StockPrice {
+  date: string;
+  price: number;
+}
+
+interface SumaryData {
+  title: string;
+  subTile: string;
+  content: string;
+}
+interface Props {
+  SumaryData?: SumaryData;
+  StockPrice?: StockPrice[];
+}
+/**
+ * 메인 페이지
+ * @param {string} ReportData - AI 생성 리포트 데이터
+ * @param {StockPrice[]} StockPrice - 주식 차트 데이터
+ * @returns
+ */
+export default function Report({ SumaryData }: Props) {
   return (
-    <div className="w-full h-[1000px] ">
-      <div className="h-[300px] w-[300px]">
-        <CardWrap>
-          <h1>
-            안녕하세요안녕하세요안녕하세요안녕하세요안녕하세요안녕하세요안녕하세요안녕하세요안녕하세요안녕하세요
-          </h1>
-        </CardWrap>
+    <div className=" w-[1200px] mx-auto py-12  ">
+      <div className="flex flex-col flex-wrap gap-6">
+        {/* 주식  헤더 영역  */}
+        <StockHeader />
+        {/* 첫번째 줄 */}
+        <article className="flex justify-between flex-wrap ">
+          <CardWrap width="488px" height="256px" padding>
+            <StockSumary />
+          </CardWrap>
+          <CardWrap width="690px" height="256px" padding>
+            <StockChart />
+          </CardWrap>
+        </article>
+
+        {/* 두번째 줄 */}
+        <article className="flex justify-between flex-wrap">
+          <CardWrap width="429px" height="297px" padding>
+            <StockReport />
+          </CardWrap>
+          <CardWrap width="750px" height="297px" padding>
+            <StockAnalysis />
+          </CardWrap>
+        </article>
       </div>
     </div>
   );
