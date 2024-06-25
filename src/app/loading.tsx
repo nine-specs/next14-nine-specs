dbcff546e59c7ae2ee42b3f56b4634fe1ab4c282
Jo Young <githuborigin@gmail.com>
2024-06-25 17:04:07 +0900

    refactor : 로딩 페이지 설명 및 스타일  설정

--- src/app/loading.tsx
+++ src/app/loading.tsx
@@ -1,8 +1,15 @@
+import CardWrap from "@/common/CardWrap";
+
 export default function loading() {
   return (
-    <div>
-      <h1>로딩 페이지</h1>
-      <p> 로딩중 일때 표시되는 페이지 입니다.</p>
-    </div>
+    <section className="flex items-center w-[100vw] h-[100vh] justify-center">
+      <CardWrap>
+        <div className="flex w-full h-full justify-center items-center">
+          <h2>root loadin.page 입니다.</h2>
+          <p>페이지가 로드 될 때 표시 됩니다.</p>
+          {/*  추후에 로딩 스피너로 대체 */}
+        </div>
+      </CardWrap>
+    </section>
   );
 }
