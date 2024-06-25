a2e6e47111a9f603c6eab392993c710f857a0388
LEE SUL <suerish.e@gmail.com>
2024-06-26 01:46:39 +0900

    ✨ feat: 챗봇 버튼에 form 상태 연결

--- src/common/Chatbot/ChatIconButton.tsx
+++ src/common/Chatbot/ChatIconButton.tsx
@@ -1,11 +1,20 @@
+"use client";
+import { useState } from "react";
 import CHATBOT_ICON from "../../../public/images/Chatbot_icon.svg";
+import ChatForm from "./ChatForm";
 
 export default function ChatIconButton() {
+  const [showChat, setShowChat] = useState(false);
+
   return (
     <>
-      <button className="hover:animate-[wiggle_1s_ease-in-out_infinite] fixed right-16 bottom-9 w-20 h-20 bg-primary-900 flex items-center justify-center rounded-full shadow-[0_0_20px_0_rgba(24,37,76,0.5)]">
+      <button
+        onClick={() => setShowChat(true)}
+        className="hover:animate-[wiggle_1s_ease-in-out_infinite] fixed right-16 bottom-9 w-20 h-20 bg-primary-900 flex items-center justify-center rounded-full shadow-[0_0_20px_0_rgba(24,37,76,0.5)]"
+      >
         <CHATBOT_ICON />
       </button>
+      {showChat && <ChatForm onClose={() => setShowChat(false)} />}
     </>
   );
 }
