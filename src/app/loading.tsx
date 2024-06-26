512bb43c2c9a84df2c0da44a995bfae6eed5dd83
Jo Young <githuborigin@gmail.com>
2024-06-26 09:21:00 +0900

    fix: 로딩 페이지 오타 수정 및 카드 스타일 적용

--- src/app/loading.tsx
+++ src/app/loading.tsx
@@ -3,11 +3,11 @@ import CardWrap from "@/common/CardWrap";
 export default function loading() {
   return (
     <section className="flex items-center w-[100vw] h-[100vh] justify-center">
-      <CardWrap>
+      <CardWrap width="256px" height="256px">
         <div className="flex w-full h-full justify-center items-center">
-          <h2>root loadin.page 입니다.</h2>
+          <h2>Root Loading page 입니다.</h2>
           <p>페이지가 로드 될 때 표시 됩니다.</p>
-          {/*  추후에 로딩 스피너로 대체 */}
+          <p>추후에 로딩 스피너로 대체.</p>
         </div>
       </CardWrap>
     </section>
