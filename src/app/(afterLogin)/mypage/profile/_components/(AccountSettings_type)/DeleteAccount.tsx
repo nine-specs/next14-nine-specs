7651e0e5423458a06953d491661246ed88d40d79
JH744 <dydekffrl@naver.com>
2024-06-27 14:47:07 +0900

    ✨ feat : 회원탈퇴사유 dropdown 구현

--- src/app/(afterLogin)/mypage/profile/_components/(AccountSettings_type)/DeleteAccount.tsx
+++ src/app/(afterLogin)/mypage/profile/_components/(AccountSettings_type)/DeleteAccount.tsx
@@ -3,8 +3,9 @@ import HeadingFont from "@/common/HeadingFont";
 import Input from "@/common/Input";
 import { Modal } from "@/common/Modal";
 import TextButton from "@/common/TextButton";
-import React, { Dispatch, SetStateAction } from "react";
-
+import React, { Dispatch, SetStateAction, useState } from "react";
+import DownIcon from "/public/images/Down_icon.svg";
+import UpIcon from "/public/images/Up_icon.svg";
 interface TDeleteAccount {
   onClose: () => void;
   setModalHandler: Dispatch<SetStateAction<string>>;
@@ -14,9 +15,19 @@ export default function DeleteAccount({
   onClose,
   setModalHandler,
 }: TDeleteAccount) {
+  const [showDropDown, setShowDropDown] = useState(false);
+  const [reason, setReason] = useState("탈퇴 사유를 선택해주세요"); // 탈퇴 사유
+
+  /**드롭다운 show 이벤트 */
+  const onClick = (e: React.MouseEvent<HTMLSpanElement>) => {
+    setShowDropDown(!showDropDown);
+    console.log(showDropDown + "클릭");
+  };
+
   // 드롭다운 옵션 선택시  발생이벤트
   const selectOption = (e: React.MouseEvent<HTMLSpanElement>) => {
-    alert("!");
+    let target = e.target as HTMLSpanElement;
+    setReason(target.innerText);
   };
 
   return (
@@ -36,82 +47,101 @@ export default function DeleteAccount({
               >
                 회원탈퇴 사유
               </BodyFont>
-              <Input value="탈퇴 사유를 선택해주세요"></Input>
-              {/* 셀렉트박스 */}
-              <div className="flex flex-col justify-center items-start gap-0 z-10 absolute border border-grayscale-300 rounded-lg w-[386px] h-[286px] bg-grayscale-0 mt-1">
-                <div className="flex-grow w-full  hover:bg-gray-100 transition duration-50 box-border py-4 pl-4 pr-[56px] ">
-                  <div
-                    className="w-[314px] h-[24px] cursor-pointer"
-                    onClick={selectOption}
-                  >
-                    <BodyFont
-                      level="4"
-                      weight="regular"
-                      className="text-gray-900"
-                    >
-                      <span className="m-0 w-0">
-                        이용이 불편하고 장애가 많아서
-                      </span>
-                    </BodyFont>
-                  </div>
-                </div>
-                <div className="flex-grow w-full  hover:bg-gray-100 transition duration-200 box-border py-4 pl-4 pr-[56px] ">
-                  <div
-                    className="w-[314px] h-[24px] cursor-pointer"
-                    onClick={selectOption}
-                  >
-                    <BodyFont
-                      level="4"
-                      weight="regular"
-                      className="text-gray-900"
-                    >
-                      <span className="m-0 w-0">다른 서비스가 더 좋아서</span>
-                    </BodyFont>
-                  </div>
-                </div>
-                <div className="flex-grow w-full  hover:bg-gray-100 transition duration-200 box-border py-4 pl-4 pr-[56px] ">
-                  <div
-                    className="w-[314px] h-[24px] cursor-pointer"
-                    onClick={selectOption}
-                  >
-                    <BodyFont
-                      level="4"
-                      weight="regular"
-                      className="text-gray-900"
-                    >
-                      <span className="m-0 w-0">사용 빈도가 낮아서</span>
-                    </BodyFont>
-                  </div>
-                </div>
-                <div className="flex-grow w-full  hover:bg-gray-100 transition duration-200 box-border py-4 pl-4 pr-[56px] ">
-                  <div
-                    className="w-[314px] h-[24px] cursor-pointer"
-                    onClick={selectOption}
-                  >
-                    <BodyFont
-                      level="4"
-                      weight="regular"
-                      className="text-gray-900"
-                    >
-                      <span className="m-0 w-0">콘텐츠 불만</span>
-                    </BodyFont>
-                  </div>
-                </div>
-                <div className="flex-grow w-full  hover:bg-gray-100 transition duration-200 box-border py-4 pl-4 pr-[56px] ">
-                  <div
-                    className="w-[314px] h-[24px] cursor-pointer"
-                    onClick={selectOption}
-                  >
-                    <BodyFont
-                      level="4"
-                      weight="regular"
-                      className="text-gray-900"
-                    >
-                      <span className="m-0 w-0">기타</span>
-                    </BodyFont>
-                  </div>
-                </div>
+              <div className="w-auto h-auto relative ">
+                <Input value={`${reason}`}></Input>
+                <button
+                  className="absolute  top-1/2 right-2 transform -translate-x-1/2 -translate-y-1/2"
+                  onClick={onClick}
+                >
+                  {/* {showDropDown ? <DownIcon /> : <UpIcon />} */}
+                  <DownIcon />
+                </button>
               </div>
+              {/* 셀렉트박스 */}
+              {showDropDown && (
+                <div className="flex flex-col justify-center items-start gap-0 z-10 absolute border border-grayscale-300 rounded-lg w-[386px] h-[286px] bg-grayscale-0 mt-1">
+                  <div className="flex-grow w-full  hover:bg-gray-100  box-border py-4 pl-4 pr-[56px] ">
+                    <div
+                      className="w-[314px] h-[24px] cursor-pointer"
+                      onClick={selectOption}
+                    >
+                      <BodyFont
+                        level="4"
+                        weight="regular"
+                        className="text-gray-900"
+                      >
+                        <span className="m-0 w-0" id="reaseon1">
+                          이용이 불편하고 장애가 많아서
+                        </span>
+                      </BodyFont>
+                    </div>
+                  </div>
+                  <div className="flex-grow w-full  hover:bg-gray-100 transition duration-200 box-border py-4 pl-4 pr-[56px] ">
+                    <div
+                      className="w-[314px] h-[24px] cursor-pointer"
+                      onClick={selectOption}
+                    >
+                      <BodyFont
+                        level="4"
+                        weight="regular"
+                        className="text-gray-900"
+                      >
+                        <span className="m-0 w-0" id="reaseon2">
+                          다른 서비스가 더 좋아서
+                        </span>
+                      </BodyFont>
+                    </div>
+                  </div>
+                  <div className="flex-grow w-full  hover:bg-gray-100 transition duration-200 box-border py-4 pl-4 pr-[56px] ">
+                    <div
+                      className="w-[314px] h-[24px] cursor-pointer"
+                      onClick={selectOption}
+                    >
+                      <BodyFont
+                        level="4"
+                        weight="regular"
+                        className="text-gray-900"
+                      >
+                        <span className="m-0 w-0" id="reaseon3">
+                          사용 빈도가 낮아서
+                        </span>
+                      </BodyFont>
+                    </div>
+                  </div>
+                  <div className="flex-grow w-full  hover:bg-gray-100 transition duration-200 box-border py-4 pl-4 pr-[56px] ">
+                    <div
+                      className="w-[314px] h-[24px] cursor-pointer"
+                      onClick={selectOption}
+                    >
+                      <BodyFont
+                        level="4"
+                        weight="regular"
+                        className="text-gray-900"
+                      >
+                        <span className="m-0 w-0" id="reaseon4">
+                          콘텐츠 불만
+                        </span>
+                      </BodyFont>
+                    </div>
+                  </div>
+                  <div className="flex-grow w-full  hover:bg-gray-100 transition duration-200 box-border py-4 pl-4 pr-[56px] ">
+                    <div
+                      className="w-[314px] h-[24px] cursor-pointer"
+                      onClick={selectOption}
+                    >
+                      <BodyFont
+                        level="4"
+                        weight="regular"
+                        className="text-gray-900"
+                      >
+                        <span className="m-0 w-0" id="reaseon5">
+                          기타
+                        </span>
+                      </BodyFont>
+                    </div>
+                  </div>
+                </div>
+              )}
               {/* 셀렉트박스 끝*/}
               <BodyFont
                 level="4"
