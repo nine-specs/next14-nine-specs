b3a0652cfcbe56c9293108e8f4fed0977b98fe7d
Jo Young <githuborigin@gmail.com>
2024-06-28 13:16:53 +0900

    fix : 렌더링 에러 해결 리차트의 container 컴포넌트 설정 제거

--- src/app/components/Report/StockChart/StockAreaChart.tsx
+++ src/app/components/Report/StockChart/StockAreaChart.tsx
@@ -4,11 +4,13 @@ import BodyFont from "@/common/BodyFont";
 import { useState } from "react";
 import { Area, AreaChart, ResponsiveContainer, Tooltip, XAxis } from "recharts";
 import PeriodRadioButton from "./PeriodRadioButton";
+import dayjs from "dayjs";
 
 interface StockAreaChartProps {
   data?: any;
 }
 
+type Period = "1일" | "3개월" | "1년" | "3년" | "10년";
 export default function StockAreaChart({ data }: StockAreaChartProps) {
   data = [
     {
@@ -132,11 +134,53 @@ export default function StockAreaChart({ data }: StockAreaChartProps) {
       closePrice: 132.0,
     },
   ];
-  const [selectedValue, setSelectedValue] = useState<string>("1일");
+  const [selectedValue, setSelectedValue] = useState<Period>("1일"); // 선택된 기간
+  const [chartData, setChartData] = useState(data); // 차트 데이터
+  const today = dayjs().format("YYYYMMDD"); // 오늘 날짜
 
+  // 계산 함수
+  const filterData = (
+    startDate: string = "20240501",
+    period: string = "1일",
+    data: any[] = [],
+  ) => {
+    const start = dayjs(startDate, "YYYYMMDD");
+
+    let end;
+    switch (period) {
+      case "1일":
+        end = start.add(1, "day");
+        break;
+      case "3개월":
+        end = start.add(3, "month");
+        break;
+      case "1년":
+        end = start.add(1, "year");
+        break;
+      case "3년":
+        end = start.add(3, "years");
+        break;
+      case "10년":
+        end = start.add(10, "years");
+        break;
+      default:
+        throw new Error("Unsupported period");
+    }
+
+    // 기간 내의 데이터 필터링
+    // isAfter : 이후 날짜, isBefore : 이전 날짜
+    return data.filter((d) => {
+      const date = dayjs(d.localDate, "YYYYMMDD");
+      return date.isAfter(start) && date.isBefore(end);
+    });
+  };
+
+  // 액션 함수
   const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
     console.log(event.target.value);
-    setSelectedValue(event.target.value);
+    setSelectedValue(event.target.value as Period); // 선택된 기간 변경
+    const selectData = filterData(today, event.target.value, data); // 기간에 따른 데이터 필터링
+    setChartData(selectData); // 차트 데이터 변경
   };
 
   const midIndex = Math.floor(data.length / 2);
@@ -147,19 +191,17 @@ export default function StockAreaChart({ data }: StockAreaChartProps) {
         <BodyFont level="1" weight="bold">
           주가 차트
         </BodyFont>
-        <ResponsiveContainer width={556} height={136}>
-          <AreaChart width={556} height={136} data={data}>
-            {/* 수정 되어야 함 */}
-            <XAxis minTickGap={10} stroke="" dataKey="localDate" />
-            <Tooltip />
-            <Area
-              type="monotone"
-              dataKey="closePrice"
-              stroke="#47B4E1"
-              fill="rgba(71, 180, 225, 0.1)"
-            />
-          </AreaChart>
-        </ResponsiveContainer>
+        <AreaChart width={556} height={136} data={chartData}>
+          {/* 수정 되어야 함 */}
+          {/* <XAxis minTickGap={10} stroke="" dataKey="localDate" /> */}
+          <Tooltip />
+          <Area
+            type="monotone"
+            dataKey="closePrice"
+            stroke="#47B4E1"
+            fill="rgba(71, 180, 225, 0.1)"
+          />
+        </AreaChart>
       </div>
 
       {/* 기간 선택 라디오 버튼  */}
