a3a5c2a4ac6d66756443cf27f0cb72ea6712fffb
최정훈 <jeongssi94@gmail.com>
2024-06-29 20:23:54 +0900

    refactor:아이디 찾기 조건 추가

--- src/app/(beforeLogin)/search/searchId/_component/SearchId.tsx
+++ src/app/(beforeLogin)/search/searchId/_component/SearchId.tsx
@@ -2,6 +2,7 @@
 import BodyFont from "@/common/BodyFont";
 import Input from "@/common/Input";
 import TextButton from "@/common/TextButton";
+import useFormValidation from "@/hooks/search/useFormValidation";
 import { registeredId } from "@/hooks/search/useSearchId";
 import { useState } from "react";
 
@@ -11,22 +12,19 @@ type SearchResultState = {
 };
 
 export default function SearchId() {
-  const [name, setName] = useState("");
-  const [phone, setPhone] = useState("");
+  const { fields, isButtonDisabled, handleFieldChange } = useFormValidation();
+
   const [searchResult, setSearchResult] = useState<SearchResultState>({
     success: true,
     userData: null,
   });
 
-  const activate = name && phone;
-
   const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
     const formData = new FormData();
-    formData.append("name", name);
-    formData.append("phone", phone);
+    formData.append("name", fields.name || "");
+    formData.append("phone", fields.phone || "");
 
-    // 타입 단언을 사용하여 result 객체의 타입을 명시적으로 지정
     const result = (await registeredId(formData)) as {
       success: boolean;
       userData?: { userId: string; createdAt: string };
@@ -44,7 +42,6 @@ export default function SearchId() {
         userData: result.userData,
       });
 
-      // Navigate programmatically
       const { userId, createdAt } = result.userData;
       const url = `/search/name=${userId}created=${createdAt}`;
       window.location.href = url;
@@ -53,7 +50,7 @@ export default function SearchId() {
         success: result.success,
         userData: null,
       });
-      const urls = `/search/searchedId`; //일단 이동은 시켜야되니까 이쪽으로 보내줌
+      const urls = `/search/searchedId`;
       window.location.href = urls;
     }
   };
@@ -65,35 +62,32 @@ export default function SearchId() {
         className="flex flex-col items-center justify-start m-0 w-[590px] h-[564px] shadow-[0px_0px_10px_5px_rgba(203,_203,_203,_0.25)] rounded-[32px] bg-grayscale-0 py-20 pr-5 pl-[22px] box-border gap-[16px] max-w-full mq725:pt-[52px] mq725:pb-[52px] mq725:box-border"
       >
         <div className="w-[386px] flex flex-col items-center justify-start gap-[40px] max-w-full mq450:gap-[20px]">
-          {/* 타이틀 영역 시작 */}
           <BodyFont level="1" weight="bold" className="text-primary-900">
             아이디 찾기
           </BodyFont>
-          {/* 타이틀 영역 끝 */}
           <div className="self-stretch flex flex-col items-start justify-start gap-[16px]">
-            {/* 이름 입력란 시작 */}
             <Input
               placeholder="이름을 입력해주세요"
               label="이름"
               name="name"
-              value={name}
-              onChange={(e) => setName(e.target.value)}
+              value={fields.name || ""}
+              onChange={(e) => handleFieldChange("name", e.target.value)}
             />
-            {/* 이름 입력란 끝 */}
-
-            {/* 전화번호 입력란 시작 */}
             <Input
-              placeholder="전화번호를 입력해주세요"
+              placeholder="-를 제외한 전화번호를 입력해주세요"
               label="휴대폰 번호"
               name="phone"
-              value={phone}
-              onChange={(e) => setPhone(e.target.value)}
+              value={fields.phone || ""}
+              onChange={(e) => handleFieldChange("phone", e.target.value)}
             />
-            {/* 전화번호 입력란 끝 */}
           </div>
         </div>
         <div className="w-[386px] flex flex-col items-start justify-start max-w-full">
-          <TextButton type="submit" variant="primary" disabled={!activate}>
+          <TextButton
+            type="submit"
+            variant="primary"
+            disabled={isButtonDisabled}
+          >
             아이디 찾기
           </TextButton>
         </div>
--- src/hooks/search/useFormValidation.ts
+++ src/hooks/search/useFormValidation.ts
@@ -4,14 +4,27 @@ const useFormValidation = () => {
   const [fields, setFields] = useState<Record<string, string>>({});
   const [isButtonDisabled, setIsButtonDisabled] = useState(true);
 
+  const validateFields = (fields: Record<string, string>) => {
+    const nameValid = /^[^\d\s~`!@#$%^&*()\-_=+[\]{}|\\;:'",.<>?/]+$/.test(
+      fields.name || "",
+    );
+    const phoneValid = /^\d{11}$/.test(fields.phone || "");
+    return nameValid && phoneValid;
+  };
+
   useEffect(() => {
     const areFieldsFilled = Object.values(fields).every(
       (field) => field.trim() !== "",
     );
-    setIsButtonDisabled(!areFieldsFilled);
+    const areFieldsValid = validateFields(fields);
+    setIsButtonDisabled(!(areFieldsFilled && areFieldsValid));
   }, [fields]);
 
   const handleFieldChange = (fieldName: string, value: string) => {
+    if (fieldName === "phone" && !/^\d{0,11}$/.test(value)) {
+      return;
+    }
+
     setFields((prevFields) => ({ ...prevFields, [fieldName]: value }));
   };
 
