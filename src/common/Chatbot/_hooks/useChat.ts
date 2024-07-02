617c2d634bdfdde8df0146fa4186cdc74231b87f
LEE SUL <suerish.e@gmail.com>
2024-07-02 15:02:19 +0900

    ✨ feat: 최초 챗일때, 기본 메세지

--- src/common/Chatbot/_hooks/useChat.ts
+++ src/common/Chatbot/_hooks/useChat.ts
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 export type MessagesType = {
   content: string;
@@ -9,6 +9,19 @@ export type MessagesType = {
 const useChat = () => {
   const [messages, setMessages] = useState<MessagesType[]>([]);
   const [input, setInput] = useState("");
+
+  useEffect(() => {
+    if (!messages.length) {
+      setMessages([
+        {
+          content:
+            "안녕하세요 아잇나우 챗봇입니다. 해외주식 관련해서 궁금하신 점이 있으면 저에게 물어보세요!",
+          role: "ai",
+          id: new Date().toLocaleString(),
+        },
+      ]);
+    }
+  }, []);
 
   const handleInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     setInput(e.target.value);
