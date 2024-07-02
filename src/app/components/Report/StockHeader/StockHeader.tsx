4b2a58dafc8d25703715820819f889efecaf9e86
Jo Young <githuborigin@gmail.com>
2024-07-02 09:02:24 +0900

    feat: 주식 종목 헤더 데이타 연동

--- src/app/components/Report/StockHeader/StockHeader.tsx
+++ src/app/components/Report/StockHeader/StockHeader.tsx
@@ -2,34 +2,28 @@ import HeadingFont from "@/common/HeadingFont";
 import BodyFont from "@/common/BodyFont";
 import StockFavorButton from "./StockFavorButton";
 import StockLogoImage from "../ReportCommon/StockLogoImage";
+import { StockInfo } from "../type/stockType";
 
 interface Props {
-  title?: string;
-  subTile?: string;
+  stockInfo: StockInfo | undefined;
 }
-/**
- * 주식 헤더 컴포넌트 주식 아이콘 , 주식 이름  , 관심종목 추가 버튼을 포함
- * @param {string} title - 주식 이름
- * @param {string} subTile - 주식 코드
- * @returns
- */
-export default function StockHeader({
-  title = "애플",
-  subTile = "AAPL",
-}: Props) {
-  return ( 
+
+export default function StockHeader({ stockInfo }: Props) {
+  if (!stockInfo) return null;
+  const { name, code, ticker } = stockInfo;
+  return (
     <article className="flex justify-between">
       <div className="flex items-center gap-3">
-        <StockLogoImage width={64} height={64} />
+        <StockLogoImage width={64} height={64} title={code} />
         <div className="flex gap-2 items-center">
           <HeadingFont level="4" weight="bold">
-            {title}
+            {name}
           </HeadingFont>
           <BodyFont level="2" weight="regular">
             ∙
           </BodyFont>
           <BodyFont level="2" weight="medium">
-            {subTile}
+            {ticker}
           </BodyFont>
         </div>
       </div>
