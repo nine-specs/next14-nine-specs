a2306f565bca33c48db9c2ab6ccfd246464ebed2
LEE SUL <suerish.e@gmail.com>
2024-07-05 17:32:26 +0900

    💄 style: 챗봇 hover 시 animation-delay

--- src/common/Chatbot/ChatIconButton.tsx
+++ src/common/Chatbot/ChatIconButton.tsx
@@ -10,7 +10,7 @@ export default function ChatIconButton() {
     <>
       <button
         onClick={() => setShowChat(true)}
-        className="hover:animate-[wiggle_1s_ease-in-out_infinite] fixed right-16 bottom-9 w-20 h-20 bg-primary-900 flex items-center justify-center rounded-full shadow-[0_0_20px_0_rgba(24,37,76,0.5)]"
+        className="hover:animate-[wiggle_1s_ease-in-out_infinite] hover:[animation-delay:-0.3s] fixed right-16 bottom-9 w-20 h-20 bg-primary-900 flex items-center justify-center rounded-full shadow-[0_0_20px_0_rgba(24,37,76,0.5)]"
       >
         <CHATBOT_ICON />
       </button>
