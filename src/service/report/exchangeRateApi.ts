a68bb63266c5548a4988dcc69cb1f475b90dbab6
Jo Young <githuborigin@gmail.com>
2024-07-05 18:09:37 +0900

    refactor: 환율 조회 api 파일명, 함수명 변경

--- src/service/report/exchangeRateApi.ts
+++ src/service/report/exchangeRateApi.ts
@@ -1,9 +1,10 @@
 /**
  * 현재 환율 정보를 가져오는 API
- * 네이버 금융에서 환율 정보를 가져옵니다. 하나은행 데이터를 사용합니다.
+ * 네이버 금융에서 원 달라 환율 정보를 가져옵니다. 하나은행 데이터를 사용합니다.
  * @returns {number} - 환율 반환
+ *  달라 원화 환율 "1,381.00"
  */
-export const exchangeApi = async () => {
+export const getExchangeRate = async () => {
   const res = await fetch(
     `http://m.stock.naver.com/front-api/marketIndex/productDetail?category=exchange&reutersCode=FX_USDKRW`,
   );
