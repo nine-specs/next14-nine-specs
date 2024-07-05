cda4a95b26b0f3db75f79dcef549b930b2edcb30
Jo Young <githuborigin@gmail.com>
2024-07-05 18:07:40 +0900

    refactor: 주식 정보 조회 api 파일 , 함수명 변경

--- src/service/report/stockDetailsApi.ts
+++ src/service/report/stockDetailsApi.ts
@@ -1,9 +1,9 @@
 /**
  * 주식 시세 변동 정보를 가져오는 API
  * @param {string} ticker 주식 코드 TSLA.O
- * @returns {object} 주식 시세 변동 정보 { closePrice: 15.9373, fluctuationsRatio: 0.041, compareToPreviousClosePrice: 0.625 }
+ * @returns {object} 주식 시세 변동 정보 배열 { closePrice: 15.9373, fluctuationsRatio: 0.041, compareToPreviousClosePrice: 0.625 }
  */
-export const stockInfoSummay = async (ticker: string) => {
+export const getStockDetails = async (ticker: string) => {
   const response = await fetch(
     `https://polling.finance.naver.com/api/realtime/worldstock/stock/${ticker}`,
     {
