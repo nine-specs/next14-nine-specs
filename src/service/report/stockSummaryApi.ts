e5ea30ff8d94f38fa4c022fa07f3b65513368950
Jo Young <githuborigin@gmail.com>
2024-07-05 18:05:27 +0900

    refactor: sumary -> summary 파일 함수 오타 수정 및 직관적인 이름 변경 환경 상수 변경에 따른 변경

--- src/service/report/stockSummaryApi.ts
+++ src/service/report/stockSummaryApi.ts
@@ -3,9 +3,9 @@
  * 네이버 주식 API를 사용하여 주식 요약을 가져온다.
  * @param stockCode 주식 코드
  * @returns 주식 요약
- * 마이크로소프트는 기술 회사다. 이 회사는 소프트웨어, 서비스, 디바이스, 솔루션을 개발하고 지원한다.<br>사업 부문은 Productivity & Business Process, Intelligent Cloud
+ * 예시 : 마이크로소프트는 기술 회사다. 이 회사는 소프트웨어, 서비스, 디바이스, 솔루션을 개발하고 지원한다.<br>사업 부문은 Productivity & Business Process, Intelligent Cloud
  */
-export const summaryApi = async (stockCode: string): Promise<string> => {
+export const getStockSummary = async (stockCode: string): Promise<string> => {
   const response = await fetch(
     `https://api.stock.naver.com/stock/${stockCode}/overview`,
   );
