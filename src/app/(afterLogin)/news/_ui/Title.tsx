d4286116d9715ac3ed90eb3d09ba56afed3bc049
dain <danggin.min@gmail.com>
2024-07-05 15:00:08 +0900

    💄 style: 타이틀 색상 수정

--- src/app/(afterLogin)/news/_ui/Title.tsx
+++ src/app/(afterLogin)/news/_ui/Title.tsx
@@ -3,7 +3,7 @@ import HeadingFont from "@/common/HeadingFont";
 export default function Title({ title }: { title: string }) {
   return (
     <>
-      <HeadingFont level="4" weight="bold" className="mb-6">
+      <HeadingFont level="4" weight="bold" className="mb-6 text-primary-900">
         {title}
       </HeadingFont>
     </>
