04779f24a1e34571cca34e2dc4bceb3b1d75194a
LEE SUL <suerish.e@gmail.com>
2024-07-08 16:34:30 +0900

    💤 chore: ICON 이름 변경

--- src/common/Chatbot/MessageBox.tsx
+++ src/common/Chatbot/MessageBox.tsx
@@ -1,6 +1,6 @@
 import BodyFont from "../BodyFont";
 import { MessagesType } from "./_hooks/useChat";
-import LOGO from "../../../public/images/logo/LOGO.svg";
+import LOGO_ICON from "../../../public/images/logo/LOGO.svg";
 
 export default function MessageBox(props: MessagesType) {
   const { role, content } = props;
@@ -23,7 +23,7 @@ export default function MessageBox(props: MessagesType) {
     return (
       <div className="flex gap-2 items-start">
         <div className="w-12 h-12 bg-primary-900 flex items-center justify-center rounded-md">
-          <LOGO width="30" height="26" />
+          <LOGO_ICON width="30" height="26" />
         </div>
         <BodyFont
           level="5"
