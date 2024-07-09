a4e314fd087aed3d90b1e05300ef472e8ef8632f
LEE SUL <suerish.e@gmail.com>
2024-07-09 18:42:44 +0900

    ✨ feat: ai api reply 추가

--- src/app/api/ai/reply/route.ts
+++ src/app/api/ai/reply/route.ts
@@ -28,25 +28,15 @@ async function* streamResponse(response: Response) {
 }
 
 export async function POST(request: Request) {
-  const { userInput } = await request.json();
+  const { prompt, temperature, top_p } = await request.json();
 
   const LLAMA_REPLY_URL = process.env.LLAMA_REPLY_URL as string;
   const BASE_URL = process.env.NEXT_PUBLIC_BASE_URL as string;
 
-  const system = `You're a professional stock advisor. You provide information and advice to clients about the stock market, individual stocks, and investment strategies. Please follow these guidelines:
-  1. always provide objective, factual information.
-  2. When using stock-related terms, add a brief explanation.
-  3. Always mention the risks of investing and emphasize the importance of personal judgment.
-  4. Do not make a 'buy' or 'sell' decision on a particular stock yourself, but only provide analytical information.
-  5. Always write your answers to questions in Korean and summarize them in a maximum of three lines.
-  6. Mention that you can provide additional information or detailed explanations if needed.
-
-  All responses must be in Korean only. Do not use any other language under any circumstances. Make sure to answer that you don't know about any topics other than stocks.`;
-
   const generateBody = {
-    user_message: `<|begin_of_text|><|start_header_id|>system<|end_header_id|>${system}<|eot_id|><|start_header_id|>user<|end_header_id|>${userInput}<|eot_id|><|start_header_id|>assistant<|end_header_id|>`,
-    temperature: 0.5,
-    top_p: 0.5,
+    user_message: prompt,
+    temperature,
+    top_p,
   };
 
   const token = await (
