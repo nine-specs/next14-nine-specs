d43f5aba7308d3170600c5ce6ddb975c00b8c515
Jo Young <githuborigin@gmail.com>
2024-07-10 18:08:53 +0900

    feat: 관심종목 컴포넌트  예시 데이터 추가

--- src/components/Report/FavorStockList/FavorStockList.tsx
+++ src/components/Report/FavorStockList/FavorStockList.tsx
@@ -5,34 +5,33 @@ import CardWrap from "@/common/CardWrap";
 import { StockInfo } from "../type/report/stockType";
 import { uuid } from "uuidv4";
 interface Props {
-  stockInfo: StockInfo | undefined;
+  stockInfo: StockInfo[] | undefined;
 }
 
 /**
  * 관심종목 리스트 컴포넌트
- * 만약 관심 정보가 없다면 null 반환
- * 관심 정보 배열로 받을 예정
- * @param {Props} { stockInfo } 조회할 종목 정보들
+ * @param {Props} { stockInfo[] } 조회할 종목 정보들
  * @returns
  */
 export default async function FavorStockList({ stockInfo }: Props) {
   if (!stockInfo) return null;
-  // 현재 임시로 stockInfo를 복사하여 리스트를 만들어서 렌더링
-  // 추후에는 전달받은 stockInfo의 배열을 사용할 예정
-  // stockInfo는
-  // ticker: string;
-  // name: string;
-  // code: string;
-  const StockInfoList = [
-    { ...stockInfo },
-    { ...stockInfo },
-    { ...stockInfo },
-    { ...stockInfo },
-  ];
+  stockInfo = [
+    {
+      ticker: "AAPL",
+      name: "애플",
+      code: "AAPL.O",
+    },
+    {
+      ticker: "AAPL",
+      name: "애플",
+      code: "AAPL.O",
+    },
+  ]; // 데이터 예시
+
   return (
     <>
-      {StockInfoList &&
-        StockInfoList.map((stockInfo) => (
+      {stockInfo &&
+        stockInfo.map((stockInfo) => (
           <div className="w-[387px] mb-3" key={uuid()}>
             <CardWrap width="387px" height="304px" padding>
               <Suspense fallback={<div>관심목록 로딩중...</div>}>
