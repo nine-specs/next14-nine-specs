1eccfede9ca3f68f6357948509c31f8c96c5aa64
Jo Young <githuborigin@gmail.com>
2024-07-10 17:23:22 +0900

    feat: 에러 페이지 수정

--- src/app/(afterLogin)/report/[id]/error.tsx
+++ src/app/(afterLogin)/report/[id]/error.tsx
@@ -1,30 +1,36 @@
-'use client' // Error components must be Client Components
- 
-import { useEffect } from 'react'
- 
+"use client";
+
+import CardWrap from "@/common/CardWrap";
+import HeadingFont from "@/common/HeadingFont";
+import { useEffect } from "react";
+
 export default function Error({
   error,
   reset,
 }: {
-  error: Error & { digest?: string }
-  reset: () => void
+  error: Error & { digest?: string };
+  reset: () => void;
 }) {
   useEffect(() => {
     // Log the error to an error reporting service
-    console.error(error)
-  }, [error])
- 
+    console.error(error);
+  }, [error]);
+
   return (
-    <div>
-      <h2>Something went wrong!</h2>
-      <button
-        onClick={
-          // Attempt to recover by trying to re-render the segment
-          () => reset()
-        }
-      >
-        Try again
-      </button>
+    <div className="flex flex-col gap-3 w-full justify-center items-center">
+      <CardWrap>
+        <HeadingFont level="1" weight="bold">
+          리포트 에러 페이지
+        </HeadingFont>
+        <button
+          onClick={
+            // Attempt to recover by trying to re-render the segment
+            () => reset()
+          }
+        >
+          새로고침
+        </button>
+      </CardWrap>
     </div>
-  )
+  );
 }
