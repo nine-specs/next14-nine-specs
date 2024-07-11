289c426a808850d55e5aac766a2d25ae13772e86
Jo Young <githuborigin@gmail.com>
2024-07-11 22:07:29 +0900

    refactor : 주식 애널리스트 컴포넌트 스켈레톤 추가

--- src/components/Report/StockAnalysis/StockAnalysisSkeleton.tsx
+++ src/components/Report/StockAnalysis/StockAnalysisSkeleton.tsx
@@ -1,10 +1,10 @@
 export default function StockAnalysisSkeleton() {
   return (
     <div className="flex flex-col justify-around  h-[96px]">
-      <div className="h-4 bg-slate-200 animate-pulse w-full rounded-md" />
+      <div className="h-4 bg-slate-200 animate-pulse w-[80%] rounded-md" />
+      <div className=" h-4 bg-slate-200 animate-pulse w-[85%] rounded-md" />
       <div className=" h-4 bg-slate-200 animate-pulse w-full rounded-md" />
-      <div className=" h-4 bg-slate-200 animate-pulse w-full rounded-md" />
-      <div className=" h-4 bg-slate-200 animate-pulse w-full rounded-md" />
+      <div className=" h-4 bg-slate-200 animate-pulse w-[90%]  rounded-md" />
     </div>
   );
 }
