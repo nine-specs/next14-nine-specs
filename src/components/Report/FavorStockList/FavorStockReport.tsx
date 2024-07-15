925a73efd9cc06dc782c71ef7d4d292754b13a57
Jo Young <githuborigin@gmail.com>
2024-07-15 16:08:06 +0900

    fix : 동적 임포트 로딩으로 서스펜스 처리

--- src/components/Report/FavorStockList/FavorStockReport.tsx
+++ src/components/Report/FavorStockList/FavorStockReport.tsx
@@ -3,12 +3,54 @@ import StockPolarChart from "../StockReport/StockPolarChart";
 import StockReportList from "../StockReport/StockReportList";
 import { getLlamaReply } from "@/service/report/llamaReplyApi";
 import { reportPrompt } from "@/constants/Report/ReportPrompt";
+import { StockReportScore } from "../type/report/stockType";
 
-interface Props {
-  code: string;
+interface StockReport {
+  scores?: StockReportScore[];
 }
+export const StockReport = ({
+  scores = [
+    {
+      subject: "주가",
+      score: 0,
+      fullMark: 100,
+    },
+    {
+      subject: "투자지수",
+      score: 0,
+      fullMark: 100,
+    },
+    {
+      subject: "수익성",
+      score: 0,
+      fullMark: 100,
+    },
+    {
+      subject: "성장성",
+      score: 0,
+      fullMark: 100,
+    },
+    {
+      subject: "관심도",
+      score: 0,
+      fullMark: 100,
+    },
+  ],
+}: StockReport) => {
+  return (
+    <div className="flex justify-between items-center ">
+      <div className=" w-[155px] h-[155px] ">
+        <StockPolarChart data={scores} />
+      </div>
+      <div className="flex-1">
+        <StockReportList data={scores} />
+      </div>
+    </div>
+  );
+};
 
-export default async function FavorStockReport({ code }: Props) {
+const fetchFavoriteData = async (code: string) => {
+  if (!code) return null;
   const token = await getLlamaToken();
   const res = await getLlamaReply({
     token: token,
@@ -19,14 +61,14 @@ export default async function FavorStockReport({ code }: Props) {
   });
   const parsedRes = JSON.parse(res);
   const { scores } = parsedRes;
-  return (
-    <div className="flex justify-between items-center ">
-      <div className=" w-[155px] h-[155px] ">
-        <StockPolarChart data={scores} />
-      </div>
-      <div className="flex-1"> 
-        <StockReportList data={scores} />
-      </div>
-    </div>
-  );
+  return scores;
+};
+
+interface Props {
+  code: string;
+}
+
+export default async function FavorStockReport({ code }: Props) {
+  const parsedRes = await fetchFavoriteData(code);
+  return <StockReport scores={parsedRes} />;
 }
