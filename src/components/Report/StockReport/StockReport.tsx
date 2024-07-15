ee70d1f9270e9ebeb08d5fc20471545d88de1894
Jo Young <githuborigin@gmail.com>
2024-07-15 13:22:03 +0900

    refactor :주식 리포트 디폴트 값 설정

--- src/components/Report/StockReport/StockReport.tsx
+++ src/components/Report/StockReport/StockReport.tsx
@@ -13,8 +13,67 @@ const StockPolarChart = dynamic(() => import("./StockPolarChart"), {
 interface Props {
   code?: string;
 }
+interface StockReportProps {
+  overallScore: number | string;
+  scores: {
+    subject: string;
+    score: number;
+    fullMark: number;
+  }[];
+}
+const RenderStockReport = ({ overallScore, scores }: StockReportProps) => (
+  <section className="space-y-6">
+    <div className="flex justify-end">
+      <HeadingFont level="3" weight="medium" className="text-grayscale-700">
+        {overallScore}점
+      </HeadingFont>
+    </div>
+
+    <div className="relative">
+      <div className="absolute left-0 w-[210px] h-[175px] z-10">
+        <StockPolarChart data={scores} cx="45%" viewAxis={true} />
+      </div>
+      <div className="absolute right-0">
+        <StockReportList data={scores} />
+      </div>
+    </div>
+  </section>
+);
+
 export default async function StockReport({ code }: Props) {
-  if (!code) return null;
+  if (!code) {
+    const { overallScore, scores } = {
+      scores: [
+        {
+          subject: "주가",
+          score: 0,
+          fullMark: 100,
+        },
+        {
+          subject: "투자지수",
+          score: 0,
+          fullMark: 100,
+        },
+        {
+          subject: "수익성",
+          score: 0,
+          fullMark: 100,
+        },
+        {
+          subject: "성장성",
+          score: 0,
+          fullMark: 100,
+        },
+        {
+          subject: "관심도",
+          score: 0,
+          fullMark: 100,
+        },
+      ],
+      overallScore: "??",
+    };
+    return <RenderStockReport scores={scores} overallScore={overallScore} />;
+  }
   const token = await getLlamaToken();
   const res = await getLlamaReply({
     token: token,
@@ -24,24 +83,6 @@ export default async function StockReport({ code }: Props) {
     stream: false,
   });
   const { overallScore, scores } = JSON.parse(res);
-  console.log(JSON.parse(res));
 
-  return (
-    <section className="space-y-6">
-      <div className="flex justify-end ">
-        <HeadingFont level="3" weight="medium" className="text-grayscale-700">
-          {overallScore}점
-        </HeadingFont>
-      </div>
-
-      <div className="relative ">
-        <div className="absolute left-0 w-[210px] h-[175px] z-10">
-          <StockPolarChart data={scores} cx="45%" viewAxis={true} />
-        </div>
-        <div className="absolute right-0">
-          <StockReportList data={scores} />
-        </div>
-      </div>
-    </section>
-  );
+  return <RenderStockReport scores={scores} overallScore={overallScore} />;
 }
--- src/components/Report/StockReport/StockReportContainer.tsx
+++ src/components/Report/StockReport/StockReportContainer.tsx
@@ -10,36 +10,6 @@ interface Props {
 }
 
 export default function StockReportContainer({ code }: Props) {
-  const { scores, overallScore } = {
-    scores: [
-      {
-        subject: "주가",
-        score: 0,
-        fullMark: 100,
-      },
-      {
-        subject: "투자지수",
-        score: 0,
-        fullMark: 100,
-      },
-      {
-        subject: "수익성",
-        score: 0,
-        fullMark: 100,
-      },
-      {
-        subject: "성장성",
-        score: 0,
-        fullMark: 100,
-      },
-      {
-        subject: "관심도",
-        score: 0,
-        fullMark: 100,
-      },
-    ],
-    overallScore: 83.5,
-  };
   return (
     <div className="relative h-full">
       <div className="absolute ">
@@ -47,33 +17,7 @@ export default function StockReportContainer({ code }: Props) {
           종목 AI 리포트
         </BodyFont>
       </div>
-      <Suspense
-        fallback={
-          <>
-            <section className="space-y-6 ">
-              <div className="flex justify-end items-center gap-1">
-                <StockSuspenseLoading />
-                <HeadingFont
-                  level="3"
-                  weight="medium"
-                  className="text-grayscale-700"
-                >
-                  점
-                </HeadingFont>
-              </div>
-
-              <div className="relative animate-pulse ">
-                <div className="absolute left-0 w-[210px] h-[175px] z-10">
-                  <StockPolarChart data={scores} cx="45%" viewAxis={true} />
-                </div>
-                <div className="absolute right-0">
-                  <StockReportList data={scores} />
-                </div>
-              </div>
-            </section>{" "}
-          </>
-        }
-      >
+      <Suspense fallback={<StockReport />}>
         <StockReport code={code} />
       </Suspense>
     </div>
