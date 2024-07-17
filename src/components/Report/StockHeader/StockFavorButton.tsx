26129eed54c446767bcdd775a6fa630e2a4b6578
Jo Young <githuborigin@gmail.com>
2024-07-17 23:45:07 +0900

    refactor : 주식 헤더 컴포넌트 개선

--- src/components/Report/StockHeader/StockFavorButton.tsx
+++ src/components/Report/StockHeader/StockFavorButton.tsx
@@ -2,22 +2,57 @@
 import TextButton from "@/common/TextButton";
 import React, { useState } from "react";
 import { setTimeout } from "timers";
+import { StockInfo } from "../type/report/stockType";
 
 interface Props {
-  title?: string;
+  stockInfo?: StockInfo;
 }
 
-/**
- * 관심종목 추가 버튼
- * @param {string} title - 버튼 텍스트
- */
-export default function StockFavorButton({ title }: Props) {
+export default function StockFavorButton({ stockInfo }: Props) {
   const [favor, setFavor] = useState(false);
   const [loading, setLoading] = useState(false);
-  const handleFavor = () => {
+  const sendData = async ({
+    uid,
+    stockInfo,
+  }: {
+    uid: string;
+    stockInfo?: StockInfo;
+  }) => {
+    await fetch(`${process.env.NEXT_PUBLIC_BASE_URL}/api/favor`, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ uid, stockInfo }),
+    });
+  };
+
+  const fetchData = async (uid: string) => {
+    fetch(`${process.env.NEXT_PUBLIC_BASE_URL}/api/favor`, {
+      method: "GET",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ uid }),
+    });
+  };
+  const handleFavor = async () => {
     setLoading(true);
     setFavor((prev) => !prev);
     // API 호출
+    const data: {
+      uid: string;
+      stockInfo?: StockInfo;
+    } = {
+      uid: "gU8dSD4pRUHr7xAx9cgL",
+      stockInfo: stockInfo,
+    };
+    const res = await sendData(data);
+    console.log(res);
+
+    const res2 = await fetchData(data.uid);
+    console.log("res2", res2);
+
     setTimeout(() => {
       setLoading(false);
     }, 1000);
