a415402477a24f390fcbb8a9df8e12e6eae24576
LEE SUL <suerish.e@gmail.com>
2024-07-17 22:13:04 +0900

    ✨ feat: 정의되지 않은 URL에 호스트 이름을 사용하는 src 값 설정

--- next.config.mjs
+++ next.config.mjs
@@ -1,9 +1,27 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  // 캐싱 로그 확인
   logging: {
     fetches: {
       fullUrl: true,
     },
+  },
+  // 정의되지 않은 URL에 호스트 이름을 사용하는 src 값을 전달할 때 설정
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "imgnews.pstatic.net",
+        port: "",
+        pathname: "/**",
+      },
+      {
+        protocol: "https",
+        hostname: "ssl.pstatic.net",
+        port: "",
+        pathname: "/**",
+      },
+    ],
   },
   // @svgr/webpack 설정
   reactStrictMode: true,
