cc02d24e497c888f770369ece21686b0389c83a9
Jo Young <githuborigin@gmail.com>
2024-07-18 20:22:19 +0900

    refactor: 관심 종목 컴포넌트 수정

--- src/components/Report/FavorStockList/FavorStockReport.tsx
+++ src/components/Report/FavorStockList/FavorStockReport.tsx
@@ -1,8 +1,6 @@
-import { getLlamaToken } from "@/service/report/llamaTokenApi";
+import { reportPrompt } from "@/constants/report/reportPrompt";
 import StockPolarChart from "../StockReport/StockPolarChart";
 import StockReportList from "../StockReport/StockReportList";
-import { getLlamaReply } from "@/service/report/llamaReplyApi";
-import { reportPrompt } from "@/constants/Report/ReportPrompt";
 import { StockReportScore } from "../type/report/stockType";
 
 interface StockReport {
@@ -49,26 +47,32 @@ export const StockReport = ({
   );
 };
 
-const fetchFavoriteData = async (code: string) => {
-  if (!code) return null;
-  const token = await getLlamaToken();
-  const res = await getLlamaReply({
-    token: token,
-    userMessage: reportPrompt(code),
-    temperature: 0.1,
-    topP: 0.1,
-    stream: false,
+const fetchStockGptReply = async (code: string) => {
+  const { system, message } = await reportPrompt(code);
+  const res = await fetch(`${process.env.NEXT_PUBLIC_BASE_URL}/api/gpt`, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({
+      system,
+      message,
+    }),
+    cache: "no-store",
   });
-  const parsedRes = JSON.parse(res);
-  const { scores } = parsedRes;
-  return scores;
+  const responseMessage = await res.json(); // 응답 데이타가 ```json ``` 하고 시작하는 문자열이라서 json으로 변환하기 위해 문자열로 받아옴
+  const jsonStart = responseMessage.indexOf("{");
+  const jsonEnd = responseMessage.lastIndexOf("}") + 1;
+  const jsonString = responseMessage.substring(jsonStart, jsonEnd);
+  const data = JSON.parse(jsonString);
+  return data;
 };
-
 interface Props {
   code: string;
 }
 
 export default async function FavorStockReport({ code }: Props) {
-  const parsedRes = await fetchFavoriteData(code);
-  return <StockReport scores={parsedRes} />;
+  const responseMessage = await fetchStockGptReply(code);
+  const { scores } = responseMessage;
+  return <StockReport scores={scores} />;
 }
---
+++ src/components/Report/FavorStockList/FevorStockHeader.tsx
@@ -0,0 +1,57 @@
+import React from "react";
+import StockLogoImage from "../ReportCommon/StockLogoImage";
+import BodyFont from "@/common/BodyFont";
+import { StockInfo } from "../type/report/stockType";
+import { getStockDetails } from "@/service/report/stockDetailsApi";
+import StockSubRate from "../ReportCommon/StockSubRate";
+
+interface Props {
+  stockInfo: StockInfo | undefined;
+}
+export default async function FevorStockHeader({ stockInfo }: Props) {
+  if (!stockInfo) return null;
+  const { name, code, ticker } = stockInfo;
+  const priceFetch = await fetch(
+    `${process.env.NEXT_PUBLIC_BASE_URL}/api/report/price`,
+    {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ code }),
+      cache: "no-store",
+    },
+  ); // 주식 가격의 정보 주식의 가격은 항시 변동되기 때문에 캐싱 없이 실시간으로 가져와야함
+  const { closePrice, fluctuationsRatio, compareToPreviousClosePrice } =
+    await priceFetch.json();
+  return (
+    <div>
+      {/* 종목 정보 */}
+      <div className="flex items-center gap-2 ">
+        <StockLogoImage width={32} height={32} code={code} />
+        <div className="flex gap-1">
+          <BodyFont level="2" weight="bold">
+            {name}
+          </BodyFont>
+          <BodyFont level="3" weight="regular">
+            {ticker}
+          </BodyFont>
+        </div>
+      </div>
+      {/* 가격 등락 */}
+      <div className="flex items-center gap-2">
+        <div className="flex p-0.5 items-center gap-0.5">
+          <BodyFont level="4" weight="medium">
+            {`$${closePrice}`}
+          </BodyFont>
+        </div>
+        <StockSubRate
+          className="gap-2"
+          changeRate={compareToPreviousClosePrice}
+          fluctuation={fluctuationsRatio}
+          level="4"
+        />
+      </div>
+    </div>
+  );
+}
