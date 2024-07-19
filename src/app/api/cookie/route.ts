0494832843f90901402132b938f923fc5b94e7f4
LEE SUL <suerish.e@gmail.com>
2024-07-19 22:27:22 +0900

    🐛 fix: 빌드 에러

--- src/app/api/cookie/route.ts
+++ src/app/api/cookie/route.ts
@@ -3,7 +3,6 @@ import jwt from "jsonwebtoken";
 
 export async function GET(request: NextRequest) {
   try {
-    console.log("==========================================================")
     const searchParams = request.nextUrl.searchParams;
     const token = searchParams.get("token");
 
@@ -14,12 +13,8 @@ export async function GET(request: NextRequest) {
     const secret = process.env.SECRET_KEY as string; // 환경 변수에서 시크릿 키 가져오기
 
     jwt.verify(token, secret);
-    console.log("token:====",token)
-    console.log("secret:====",secret)
 
-    const response = NextResponse.redirect(
-      new URL(`/socialSign?token=${token}`, request.nextUrl.origin),
-    );
+    const response = NextResponse.redirect(new URL(`/socialSign?token=${token}`, request.nextUrl.origin));
     response.cookies.set("auth-token", token, {
       secure: process.env.NODE_ENV === "production",
       maxAge: 60 * 30, // 30분
@@ -31,3 +26,5 @@ export async function GET(request: NextRequest) {
     return NextResponse.json({ error: "Invalid token" }, { status: 401 });
   }
 }
+
+export const dynamic = "force-dynamic";
--- src/app/api/verify/route.ts
+++ src/app/api/verify/route.ts
@@ -8,36 +8,26 @@ export async function GET(request: NextRequest) {
     //console.log("토큰 가져오기: ", token);
 
     if (!token) {
-      return NextResponse.json(
-        { message: "토큰이 제공되지 않았습니다." },
-        { status: 400 },
-      );
+      return NextResponse.json({ message: "토큰이 제공되지 않았습니다." }, { status: 400 });
     }
 
     const isValid = verifyToken(token);
 
     if (!isValid) {
-      return NextResponse.json(
-        { message: "유효하지 않은 토큰입니다." },
-        { status: 400 },
-      );
+      return NextResponse.json({ message: "유효하지 않은 토큰입니다." }, { status: 400 });
     }
 
     const email = getEmailFromToken(token);
 
     if (!email) {
-      return NextResponse.json(
-        { message: "토큰에서 이메일을 추출할 수 없습니다." },
-        { status: 400 },
-      );
+      return NextResponse.json({ message: "토큰에서 이메일을 추출할 수 없습니다." }, { status: 400 });
     }
 
     return NextResponse.json({ status: 200, email }, { status: 200 });
   } catch (error) {
     console.error("토큰 검증 에러:", error);
-    return NextResponse.json(
-      { message: "토큰 검증에 실패했습니다." },
-      { status: 500 },
-    );
+    return NextResponse.json({ message: "토큰 검증에 실패했습니다." }, { status: 500 });
   }
 }
+
+export const dynamic = "force-dynamic";
