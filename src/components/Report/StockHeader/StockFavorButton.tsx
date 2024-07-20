468161f611b5988673b676316e3ab9d270460d07
Jo Young <githuborigin@gmail.com>
2024-07-20 21:31:51 +0900

    feat: 관심종목 추가 및 제거 구현

--- src/components/Report/StockHeader/StockFavorButton.tsx
+++ src/components/Report/StockHeader/StockFavorButton.tsx
@@ -1,71 +1,83 @@
 "use client";
 import TextButton from "@/common/TextButton";
-import React, { useState } from "react";
+import React, { use, useEffect, useState } from "react";
 import { setTimeout } from "timers";
 import { StockInfo } from "../type/report/stockType";
 
 interface Props {
-  stockInfo?: StockInfo;
+  stockInfo: StockInfo;
 }
+const getData = async (uid: string) => {
+  const response = await fetch(`${process.env.NEXT_PUBLIC_BASE_URL}/api/favorStock?uid=${uid}`, {
+    method: "GET",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    cache: "no-store",
+  });
+  const data = await response.json();
+  console.log(data);
+  return data;
+};
 
+const updateData = async (uid: string, stockInfo: StockInfo) => {
+  const response = await fetch(`${process.env.NEXT_PUBLIC_BASE_URL}/api/favorStock`, {
+    method: "PUT",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({ uid, stockInfo }),
+    cache: "no-store",
+  });
+  const data = await response.json();
+  console.log(data);
+  return data;
+};
 export default function StockFavorButton({ stockInfo }: Props) {
-  const [favor, setFavor] = useState(false);
+  const { name } = stockInfo;
+  const [favor, setFavor] = useState(true);
   const [loading, setLoading] = useState(false);
-  const sendData = async ({
-    uid,
-    stockInfo,
-  }: {
-    uid: string;
-    stockInfo?: StockInfo;
-  }) => {
-    await fetch(`${process.env.NEXT_PUBLIC_BASE_URL}/api/favor`, {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify({ uid, stockInfo }),
-    });
-  };
+  const uid = "test"; // 임시로 test로 설정
 
-  const fetchData = async (uid: string) => {
-    fetch(`${process.env.NEXT_PUBLIC_BASE_URL}/api/favor`, {
-      method: "GET",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify({ uid }),
-    });
+  const chageState = async (stocks: any) => {
+    if (name in stocks) {
+      setFavor(false);
+    } else {
+      setFavor(true);
+    }
   };
-  const handleFavor = async () => {
-    setLoading(true);
-    setFavor((prev) => !prev);
-    // API 호출
-    const data: {
-      uid: string;
-      stockInfo?: StockInfo;
-    } = {
-      uid: "gU8dSD4pRUHr7xAx9cgL",
-      stockInfo: stockInfo,
+  useEffect(() => {
+    const fetchAndSetData = async () => {
+      setLoading(true);
+      try {
+        const data = await getData(uid);
+        chageState(data);
+      } catch (error) {
+        console.error("Error fetching data:", error);
+      } finally {
+        setLoading(false);
+      }
     };
-    const res = await sendData(data);
-    console.log(res);
+    fetchAndSetData();
+  }, [uid, name]);
 
-    const res2 = await fetchData(data.uid);
-    console.log("res2", res2);
-
-    setTimeout(() => {
-      setLoading(false);
-    }, 1000);
+  const handleFavor = async () => {
+    const data = await updateData(uid, stockInfo);
+    chageState(data);
   };
   return (
     <div className="w-[180px]">
       <TextButton
         onClick={handleFavor}
-        variant={`${favor ? "outline" : "primary"}`}
         size="lg"
+        className={`${
+          !favor
+            ? "bg-transparent text-primary-900 hover:border-primary-800 hover:text-primary-800 border-primary-900"
+            : "bg-primary-900 hover:bg-primary-800 text-white border-primary-900"
+        }`}
         disabled={loading}
       >
-        {favor ? "관심종목 추가" : "관심종목 해제"}
+        {!favor ? "관심종목 해제" : "관심종목 추가"}
       </TextButton>
     </div>
   );
