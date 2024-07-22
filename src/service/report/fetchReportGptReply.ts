6c76156e217569607f36cc08dc9cb1932f2730e7
Jo Young <githuborigin@gmail.com>
2024-07-23 08:35:02 +0900

    feat: 파이어베이스에 일별  gpt 데이터 넣기

---
+++ src/service/report/fetchReportGptReply.ts
@@ -0,0 +1,24 @@
+import { reportPrompt } from "@/constants/report/reportPrompt";
+
+export const fetchReportGptReply = async (code: string) => {
+  const { system, message } = await reportPrompt(code);
+
+  const res = await fetch(`${process.env.NEXT_PUBLIC_BASE_URL}/api/report/gptReplyStore`, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({
+      system,
+      message,
+    }),
+    cache: "no-store",
+  });
+  const responseMessage = await res.json(); // 응답 데이타가 ```json ``` 하고 시작하는 문자열이라서 json으로 변환하기 위해 문자열로 받아옴
+  // console.log(responseMessage);
+  // const jsonStart = responseMessage.indexOf("{");
+  // const jsonEnd = responseMessage.lastIndexOf("}") + 1;
+  // const jsonString = responseMessage.substring(jsonStart, jsonEnd);
+  // const data = JSON.parse(jsonString);
+  return responseMessage;
+};
