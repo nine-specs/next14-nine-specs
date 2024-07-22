44dfd64cfafef796192a5138830ba832e876e575
dain <danggin.min@gmail.com>
2024-07-22 18:49:01 +0900

    ♻️ refactor: 크롤링 데이터 DB 저장 및 주식 종목에 따른 relatedStocks 필드 추가

--- src/service/news/crawler/getMainNews.ts
+++ src/service/news/crawler/getMainNews.ts
@@ -8,6 +8,22 @@ import { uuid } from "uuidv4";
 import { setImprovedCrawlerPerformance } from "./setImprovedCrawlerPerformance";
 import parseToTimestamp from "./parseToTimestmp";
 
+const stockList = {
+  애플: "AAPL",
+  아마존: "AMZN",
+  테슬라: "TSLA",
+  마이크로소프트: "MSFT",
+  구글: "GOOGL",
+  유니티: "U",
+  고프로: "GPRO",
+  로빈후드: "HOOD",
+  브로드컴: "AVGO",
+  엔비디아: "NVDA",
+  카사바사이언스: "SAVA",
+  코닥: "KODK",
+  펠로튼: "PTON",
+};
+
 export const getMainNews = async () => {
   const browser = await puppeteer.launch(); // 브라우저 실행
   const page = await browser.newPage(); // 새로운 페이지 열기
@@ -16,7 +32,7 @@ export const getMainNews = async () => {
     height: 1080,
   });
 
-  setImprovedCrawlerPerformance();
+  setImprovedCrawlerPerformance(page);
 
   try {
     const targetURL = "https://m.stock.naver.com/investment/news/mainnews";
@@ -39,18 +55,29 @@ export const getMainNews = async () => {
         const html = await page.content();
         const $ = cheerio.load(html);
 
+        let contents = $("._article_content").text();
+
+        // 관련 종목 설정
+        let relatedStocks = [];
+        for (const [key, value] of Object.entries(stockList)) {
+          if (contents.includes(key)) {
+            relatedStocks.push(value);
+          }
+        }
+
         const newsData: NewsResponse = {
           newsId: uuid(),
-          relatedStocks: "",
+          relatedStocks: relatedStocks[0] || "",
           headLine: $("h2#title_area span").text(),
           description: $("meta[property='og:description']").attr("content") as string,
-          contents: $("._article_content").text(),
+          contents: contents.replace(/[\t]/g, ""),
           image: $("meta[property='og:image']").attr("content") as string,
           creationTime: parseToTimestamp($("._ARTICLE_DATE_TIME").text()),
           media: $(".media_end_head_top_logo img").attr("title") as string,
-          category: $(".media_end_categorize_item").text(),
+          category: $(".media_end_categorize_item:first-child").text(),
         };
 
+        // DB에 저장
         const querySnapshot = await getDocs(collection(firestore, "news", "mainNews", "articles"));
         const existingNews = querySnapshot.docs.map((doc) => doc.data().headLine);
 
@@ -58,7 +85,7 @@ export const getMainNews = async () => {
           const mainRef = collection(firestore, "news", "mainNews", "articles");
           await addDoc(mainRef, newsData);
         } else {
-          return;
+          continue;
         }
       } catch (error) {
         console.error("Error : ", error);
