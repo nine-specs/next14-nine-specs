4226e61ac4ff87a027b328892853bfe7d7879975
dain <danggin.min@gmail.com>
2024-07-23 12:50:57 +0900

    ✨ feat: 이미지 없을 때 문구 추가

--- src/app/(afterLogin)/news/_components/FavoriteStockRelatedNewsItem.tsx
+++ src/app/(afterLogin)/news/_components/FavoriteStockRelatedNewsItem.tsx
@@ -4,6 +4,7 @@ import BodyFont from "@/common/BodyFont";
 import CardWrap from "@/common/CardWrap";
 import MoreButton from "../_ui/MoreButton";
 import useTimestampToDate from "@/hooks/common/useTimestampToDate";
+import CaptionFont from "@/common/CaptionFont";
 
 export default function FavoriteStockRelatedNewsItem(props: any) {
   const { headLine, media, image, creationTime, relatedStock } = props;
@@ -16,11 +17,13 @@ export default function FavoriteStockRelatedNewsItem(props: any) {
           {image ? (
             <Image src={image} width={388} height={236} alt="" className="object-cover object-center h-auto" />
           ) : (
-            ""
+            <div className="w-full h-full flex justify-center items-center">
+              <CaptionFont weight="regular">관련 이미지가 없습니다.</CaptionFont>
+            </div>
           )}
         </div>
         <div className="px-6 pt-4 pb-6">
-          <BodyFont level="3" weight="medium" className="text-primary-900">
+          <BodyFont level="3" weight="medium" className="text-primary-900 min-h-[60px]">
             {headLine}
           </BodyFont>
           <aside className="flex justify-between pt-2 text-grayscale-600">
