a11521b256a54cff5b525360fa6ac5fe376c711c
dain <danggin.min@gmail.com>
2024-07-24 09:39:15 +0900

    💄 style: 관련 종목 height 속성 수정

--- src/app/(afterLogin)/news/[id]/_components/RelatedStock.tsx
+++ src/app/(afterLogin)/news/[id]/_components/RelatedStock.tsx
@@ -1,13 +1,12 @@
 import BodyFont from "@/common/BodyFont";
 import ListWrap from "@/common/ListWrap";
-import RelatedStockItem from "./RelatedStockItem";
 import { StockInfo } from "@/components/Report/type/report/stockType";
 import StockItem from "@/common/StockItem/StockItem";
 
 export default function RelatedStock({ stockList }: { stockList: StockInfo[] }) {
   return (
     <>
-      <ListWrap width="382px" height="310px" padding="md" className="flex flex-col gap-5">
+      <ListWrap width="382px" height="auto" padding="md" className="flex flex-col gap-5">
         <BodyFont level="3" weight="bold" className="text-primary-900">
           현재 뉴스와 관련된 주식
         </BodyFont>
