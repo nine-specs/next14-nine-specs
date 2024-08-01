c9f2335cdeac4c071549cf4b947483e9aa629a10
dain <danggin.min@gmail.com>
2024-08-02 07:26:09 +0900

    ✨ feat: 관심 종목과 관련된 뉴스 구현

--- src/app/(afterLogin)/news/page.tsx
+++ src/app/(afterLogin)/news/page.tsx
@@ -1,15 +1,20 @@
 import RecentNews from "./_components/RecentNews";
 import FavoriteStockRelatedNews from "./_components/FavoriteStockRelatedNews";
 import PopularNews from "@/components/news/PopularNews";
+import { getSession } from "@/lib/getSession";
 
 export const dynamic = "force-dynamic";
 
-export default function NewsHome() {
+export default async function NewsHome() {
+  const session = await getSession();
+  const userId = session?.user?.id || "";
+  const type = "all";
+
   return (
     <>
       <div className="max-w-[1200px] pt-14 pb-20 overflow-hidden">
-        <PopularNews />
-        <FavoriteStockRelatedNews />
+        <PopularNews type={type} />
+        <FavoriteStockRelatedNews userId={userId} />
         <RecentNews />
       </div>
     </>
